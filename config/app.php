8c749626d4e76c8e438bf6200f0b7f7d70c3f951
Shift <shift@laravelshift.com>
2025-01-22 23:41:52 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -9,22 +9,6 @@ return [
 
     'register_url' => env('REGI_URL'),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        Barryvdh\DomPDF\ServiceProvider::class,
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'PDF' => Barryvdh\DomPDF\Facade::class,
